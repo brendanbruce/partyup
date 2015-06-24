5052a02677c9ce869fb8180e3b7d871889fc7bb2
Brendan Bruce <brendanbruce@gmail.com>
2015-06-24 00:19:56 -0400

    Add index to user email

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150618064212) do
+ActiveRecord::Schema.define(version: 20150624041051) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,5 +56,7 @@ ActiveRecord::Schema.define(version: 20150618064212) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
